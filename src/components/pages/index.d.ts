e3b3641d1316267c39583efb6a4b103c1417dd1c
ikacin <ikacin@new.ai>
2024-10-15 17:17:32 +0300

    bugfix

---
+++ src/components/pages/index.d.ts
@@ -0,0 +1,24 @@
+declare module '../src/components/pages/HomePage' {
+    const HomePage: any;
+    export default HomePage;
+}
+
+declare module '../src/components/pages/AboutPage' {
+    const AboutPage: any;
+    export default AboutPage;
+}
+
+declare module '../src/components/pages/ProjectPage' {
+    const ProjectPage: any;
+    export default ProjectPage;
+}
+
+declare module '../src/components/pages/SkillPage' {
+    const SkillPage: any;
+    export default SkillPage;
+}
+
+declare module '../src/components/pages/EducationPage' {
+    const EducationPage: any;
+    export default EducationPage;
+}
